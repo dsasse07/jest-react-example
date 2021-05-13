f6e250d9289d434ac287be20b0743e9dfa0196ad
Daniel Sasse <dsasse07@gmail.com>
2021-05-13 13:24:27 -0400

    iExample Complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [listItems, setListItems] = useState([])
+  const [newItemText, setNewItemText] = useState("")
+
+  const handleCounterClick = value => {
+    // const newValue = counter + value
+    setCounter( counter => counter + value )
+  }
+
+  const handleNewItemChange = e => {
+    setNewItemText(e.target.value)
+  }
+
+  const handleAddItem = e => {
+    e.preventDefault()
+    setListItems([...listItems, {
+      text: newItemText,id: listItems.length
+      }
+    ])
+    setNewItemText('')
+  }
+
+  const handleRemoveItem = id => {
+    const newListItems = listItems.filter( item => item.id !== id)
+    setListItems(newListItems)
+  }
+
+  const listItemComponents = listItems.map( item => {
+    return (
+      <li
+        data-testid={`item${item.id}`}
+        key={item.id}
+      >
+        {item.text}
+        <button
+          data-testid={`remove-item${item.id}`}
+          onClick={() => handleRemoveItem(item.id)}
+        >
+          Remove
+        </button>
+      </li>
+    )
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Counter:
+          <span data-testid="counter-value">
+            {counter}
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button 
+            onClick={() => handleCounterClick(1)}
+          >
+            Increment
+          </button>
+          <button 
+            onClick={() => handleCounterClick(-1)}
+            disabled={counter <= 0}
+          >
+            Decrement
+          </button>
+        </div>
+        <form onSubmit={handleAddItem}>
+          <label
+            htmlFor="newItem"
+          >
+            Create List Item
+            <input 
+              id="newItem"
+              value={newItemText}
+              onChange={handleNewItemChange}
+            />
+          </label>
+          <input
+            data-testid="add-item"
+            type="submit"
+            value="Add Item"
+          />
+        </form>
+        <ul>
+          {listItemComponents}
+        </ul>
+
+
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,89 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe( 'App Counter', () => {
+
+  test('Counter Elements should be present', () => {
+    render(<App />)
+    const incrementButton = screen.getByText(/Increment/i)
+    const decrementButton = screen.getByText(/Decrement/i)
+    const counterLabel = screen.getByText(/Counter:/i)
+    let counterText = screen.getByTestId("counter-value")
+    
+    expect(incrementButton).toBeInTheDocument()
+    expect(incrementButton).toBeEnabled()
+    expect(decrementButton).toBeInTheDocument()
+    expect(decrementButton).toBeDisabled()
+    expect(counterLabel).toBeInTheDocument()
+    expect(counterText).toHaveTextContent(0)
+  })
+
+  test('Increment increases value by 1 and enables decrement button present', () => {
+    render(<App />)
+    const incrementButton = screen.getByText(/Increment/i)
+    const decrementButton = screen.getByText(/Decrement/i)
+    const counterText = screen.getByTestId("counter-value")
+
+    expect(counterText).toHaveTextContent(0)
+    userEvent.click(incrementButton)
+    expect(counterText).toHaveTextContent(1)
+    expect(decrementButton).not.toBeDisabled()
+  })
+
+  test('Decrement decreases value by 1 and disables decrement button at 0', () => {
+    render(<App />)
+    const incrementButton = screen.getByText(/Increment/i)
+    const decrementButton = screen.getByText(/Decrement/i)
+    const counterText = screen.getByTestId("counter-value")
+
+    expect(counterText).toHaveTextContent(0)
+    userEvent.click(incrementButton)
+    expect(counterText).toHaveTextContent(1)
+    expect(decrementButton).not.toBeDisabled()
+    userEvent.click(decrementButton)
+    expect(counterText).toHaveTextContent(0)
+    expect(decrementButton).toBeDisabled()
+  })
+})
+
+describe('App Item List', () => {
+  test('List Form Components render', () => {
+    render(<App />)
+    const listItemInput = screen.getByLabelText(/Create List Item/i)
+    const addItemButton = screen.getByTestId("add-item")
+
+    expect(listItemInput).toBeInTheDocument()
+    expect(addItemButton).toBeInTheDocument()
+  })
+
+  test('User can add item to page', () => {
+    render(<App />)
+    const listItemInput = screen.getByLabelText(/Create List Item/i)
+    const addItemButton = screen.getByTestId("add-item")
+
+    expect(listItemInput).toHaveValue("")
+    userEvent.type(listItemInput, "hello")
+    expect(listItemInput).toHaveValue("hello")
+
+    userEvent.click(addItemButton)
+    expect(screen.getByText("hello")).toBeInTheDocument()
+    expect(listItemInput).toHaveValue("")
+  })
+
+  test('User can remove item from page', () => {
+    render(<App />)
+    const listItemInput = screen.getByLabelText(/Create List Item/i)
+    const addItemButton = screen.getByTestId("add-item")
+
+    userEvent.type(listItemInput, "hello")
+    userEvent.click(addItemButton)
+    const newItem = screen.getByText("hello")
+    expect(newItem).toBeInTheDocument()
+    
+    const removeButton = screen.getByTestId('remove-item0')
+    userEvent.click(removeButton)
+    expect(newItem).not.toBeInTheDocument()
+  })
+})
